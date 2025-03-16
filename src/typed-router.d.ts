57d95df78e3d341abb10049a20f2d19aabe50f7a
farbian_2005 <fabian_bernhart@web.de>
2025-03-16 12:49:07 +0100

    feat: add dashbboard route

--- src/typed-router.d.ts
+++ src/typed-router.d.ts
@@ -19,7 +19,7 @@ declare module 'vue-router/auto-routes' {
    */
   export interface RouteNamedMap {
     '/': RouteRecordInfo<'/', '/', Record<never, never>, Record<never, never>>,
+    '/dashboard': RouteRecordInfo<'/dashboard', '/dashboard', Record<never, never>, Record<never, never>>,
     '/monitor': RouteRecordInfo<'/monitor', '/monitor', Record<never, never>, Record<never, never>>,
-    '/new/': RouteRecordInfo<'/new/', '/new', Record<never, never>, Record<never, never>>,
   }
 }
